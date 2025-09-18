3281f70dce9f83c379992f5ece5ad5267a38d159
Wesley Bruno <engwesleybruno@gmail.com>
2025-09-18 09:58:18 -0300

    au

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+const { defineConfig } = require("vite");
+const react = require("@vitejs/plugin-react");
 
 // https://vitejs.dev/config/
-export default defineConfig({
+module.exports = defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // Esta é a forma correta e robusta de criar um alias.
-      // require.resolve encontra o caminho completo para o ficheiro principal do pacote,
-      // garantindo que nunca teremos o erro "file not found".
+      // Agora que o ficheiro é .cjs, o require.resolve vai funcionar perfeitamente.
       "@react-editor-js/client": require.resolve("@react-editor-js/client"),
       "react-editor-js": require.resolve("react-editor-js"),
     },
