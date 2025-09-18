4a231b4beb33f2d0685225966a8628ca54fa0158
Wesley Bruno <engwesleybruno@gmail.com>
2025-09-18 09:54:31 -0300

    ajuste

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path"; // Importe o módulo 'path' do Node.js
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ["@react-editor-js/client"],
+  resolve: {
+    alias: {
+      // Força o Vite a usar a versão de módulo ES (para o navegador) da biblioteca
+      "@react-editor-js/client": path.resolve(
+        __dirname,
+        "node_modules/@react-editor-js/client/dist/react-editor-js-client.mjs"
+      ),
+      // Adicionar um alias para a biblioteca principal por segurança
+      "react-editor-js": path.resolve(
+        __dirname,
+        "node_modules/react-editor-js/dist/react-editor-js.mjs"
+      ),
+    },
   },
 });
